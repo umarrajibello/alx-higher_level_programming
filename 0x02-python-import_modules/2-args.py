4bf419cfbccdc77b804019640c01ab53b74844cd
Umar Raji Bello <umarrajibello@gmail.com>
2024-04-19 01:16:41 +0800

    Codded args task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    print("{} argument:".format(len(argv) - 1))
-    for i in range(1, len(argv)):
-        print("{}: {}".format(i, argv[i]))
+    args = len(argv) - 1
+    if args == 0:
+        print("{} arguments.".format(args))
+    elif args == 1:
+        print("{} argument:\n{}: {}".format(args, args, argv[args]))
+    else:
+        print("{} arguments:".format(args))
+        for i in range(1, args + 1):
+            print("{}: {}".format(i, argv[i]))
